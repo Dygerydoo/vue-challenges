43b9066f46dcc252a78463bc897503d45b15fc48
Dygerydoo <caserofrancisco@gmail.com>
2017-11-04 13:55:16 +0100

    Create todo list header content

--- todo/main.js
+++ todo/main.js
@@ -4,3 +4,27 @@ var setDate = new Date(),
     readableDate = setDate.toLocaleDateString(locale, dateOptions);
 
 var todayDate = readableDate.charAt(0).toUpperCase() + readableDate.slice(1);
+
+new Vue({
+  el: '#app',
+  data: {
+    todoDate: todayDate,
+    taskList: [
+      'Hacer la compra',
+      'Pasear al perro',
+      'Llevar el coche al taller',
+      'Comprarme camisetas',
+    ],
+    taskItem: '',
+  },
+  methods: {
+    addTask: function() {
+      if (this.taskItem === '') {
+        alert('Debes escribir una tarea');
+      } else {
+        this.taskList.push(this.taskItem);
+        this.taskItem = '';
+      }
+    }
+  }
+});
