f595ae810f2e97dd71b374827697a964ee299854
Dygerydoo <caserofrancisco@gmail.com>
2017-12-03 18:19:43 +0100

    Album artwork and play song from search list

--- music-player/src/store/store.js
+++ music-player/src/store/store.js
@@ -6,6 +6,9 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     searchResults: [],
+    isPlaying: false,
+    isPaused: false,
+    selectedSong: {},
   },
   getters: {
     getSearchResults: state => {
@@ -15,11 +18,19 @@ export const store = new Vuex.Store({
   mutations: {
     FILL_SEARCH_LIST(state, searchResults) {
       state.searchResults = searchResults;
+    },
+    PLAY_SONG(state, song) {
+      state.isPlaying = true;
+      state.isPaused = false;
+      state.selectedSong = song;
     }
   },
   actions: {
     fillSearchList({commit}, searchResults) {
       commit('FILL_SEARCH_LIST', searchResults);
+    },
+    playSong({commit}, song) {
+      commit('PLAY_SONG', song);
     }
   }
 });
